db6a1d7ed6a0dc384918f6eaed9b3719f5a827ef
moonwalker <pestefighter@icloud.com>
2018-10-30 00:17:22 +0900

    sorry but you can't use this exchange

--- Deepanc_sepa.py
+++ Deepanc_sepa.py
@@ -10,7 +10,8 @@ parser = OptionParser(usage = "usage% option <file>")
     parser.options("-L","--ignore-bound",dest = "limit_bounds"
         default = True ,action = "store_false"
         help = "this is the first option of the test")
-    parsee.options("-s","--dont-get")
+    parsee.options("-s","--dont-get"
+        default = False)
 
 slide = OpenSlide("test.svs")
 #  this is opneslide reader of the testslide which the format is svs
