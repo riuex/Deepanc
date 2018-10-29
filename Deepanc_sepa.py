90e635e214c49d48975cca1ee38beb994ef6d1e3
moonlupus <pestemoonwalk@icloud.com>
2018-10-30 04:42:28 +0900

    good morning!!
    this is processing report

--- Deepanc_sepa.py
+++ Deepanc_sepa.py
@@ -3,21 +3,20 @@ import os
 import option
 import openslide
 from openslide import open_slide,openslide
-
+#this package is need to read SVSfiles.
+from multiprocessing import pool
+import multiprocessing as multi
+#this package is need to make multitask possible
 
 
 parser = OptionParser(usage = "usage% option <file>")
-    parser.options("-L","--ignore-bound",dest = "limit_bounds"
+parser.options("-L","--ignore-bound",dest = "limit_bounds"
         default = True ,action = "store_false"
         help = "this is the first option of the test")
-    parsee.options("-s","--dont-get"
+parsee.options("-s","--dont-get"
         default = False)
 
 slide = OpenSlide("test.svs")
-"""
 #  this is opneslide reader of the testslide which the format is svs
+
 testjpg = slide.read_region((0,0),0,slide.dimensions)
-# this line is ignored
-"""
-#how about this change
-#and how about this?
