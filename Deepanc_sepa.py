0f7dd75eaf493fc37059938dd8f8c6cb34da9a58
riuex <pestefighter@gmail.com>
2018-10-29 22:00:20 +0900

    todai
    you can't write this jpeg file because of enlarged file

---
+++ Deepanc_sepa.py
@@ -0,0 +1,17 @@
+import numpy
+import os
+import option
+import openslide
+from openslide import open_slide,openslide
+
+
+
+parser = OptionParser(usage = "usage% option <file>")
+    parser.options("-L","--ignore-bound",dest = "limit_bounds"
+        default = True ,action = "store_false"
+        help = "this is the first option of the test")
+    parsee.options()
+
+slide = OpenSlide("test.svs")
+#  this is opneslide reader of the testslide which the format is svs
+testjpg = slide.read_region((0,0),0,slide.dimensions)
