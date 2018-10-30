13f7c6cf1315ecb2498cb808de7b5187ec912cea
moonlupus <pestemoonwalk@icloud.com>
2018-10-30 18:38:25 +0900

    This is the last of today

--- Deepanc_sepa.py
+++ Deepanc_sepa.py
@@ -24,37 +24,56 @@ def convert(data,args.pixel):
         w_end,h_end = w%UNIT_X,h%UNIT_Y
         w_size,h_size = UNIT_X,UNIT_Y
         w_start,h_start = 0,0
-        
+        v_lst = []
+        for i in range(h_rep):
+            if i == h_rep - 1:
+                h_size = h_end
+            h_lst = []
+            for j in range(w_rep):
+                if j == w_rep - 1:
+                    w_size = w_end
+                img = simage.read_region((W_start,h_start),0,(w_size,h_size))
+                img = img.convert("RGB")
+                h_list.append(h_list)
+                w_start += UNIT_X
+            v_lst.append(h_lst)
+            w_size = UNIT_X
+            h_start += UNIT_Y
+            w_start = 0
 
     except:
-        print("we can't process " + fname)
+        print("Can't open image file : " + fname)
     return
 
+if __name__ == "__main__":
+    try:
+        #Read the command line.
+        parser = OptionParser(usage = "usage% option <file>")
+        parser.options("-L","--ignore-bound",dest = "limit_bounds"
+            default = True ,action = "store_false"
+            help = "this is the first option of the test")
+        parsee.options("-m","--multi",type = int,
+            default = 2,
+            help = "you should input your number of CPUcores")
+        parser.options("-p","--pixel",type = int,
+            default = 512,
+            help = "please input pixels of sepalated panel")
+        parse.options("-i","--input",default = "",
+            help = "you should input the directory of the SVSfiles")
+        parser.options("-o","--output",default = "",
+            help = "you should input the name of output")
 
-parser = OptionParser(usage = "usage% option <file>")
-parser.options("-L","--ignore-bound",dest = "limit_bounds"
-    default = True ,action = "store_false"
-    help = "this is the first option of the test")
-parsee.options("-m","--multi",type = int,
-    default = 2,
-    help = "you should input your number of CPUcores")
-parser.options("-p","--pixel",type = int,
-    default = 512,
-    help = "please input pixels of sepalated panel")
-parse.options("-i","--input",default = "",
-    help = "you should input the directory of the SVSfiles")
-parser.options("-o","--output",default = "",
-    help = "you should input the name of output")
-
-
-args = parser.parse_args()
-# "args" is object which contains all of parameter which user definded on command line
-slide = OpenSlide("test.svs")
-#  this is opneslide reader of the testslide which the format is svs
-#
-f_list = [f for f in os.listdir[args.input] if ".svs" in f]
-f_list = [[f,args.input,args.output] for f in f_list]
-
-p = pool(args.multi)
-p.map(convert,file)
-#testjpg = slide.read_region((0,0),0,slide.dimensions)
+        args = parser.parse_args()
+        # "args" is object which contains all of parameter which user definded on command line
+        slide = OpenSlide("test.svs")
+        #  this is opneslide reader of the testslide which the format is svs
+        #
+        f_list = [f for f in os.listdir[args.input] if ".svs" in f]
+        f_list = [[f,args.input,args.output] for f in f_list]
+        print("----------program start----------")
+        p = pool(args.multi)
+        #Set multi processing and run.
+        p.map(convert,file)
+        #testjpg = slide.read_region((0,0),0,slide.dimensions)
+    except:
+        print("This command was failed")
