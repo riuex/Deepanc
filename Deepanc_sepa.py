81746e6904328f16bc15910db544d8b69ca00db7
moonlupus <pestemoonwalk@icloud.com>
2018-11-01 12:52:51 +0900

    Update Deepanc_sepa.py

--- Deepanc_sepa.py
+++ Deepanc_sepa.py
@@ -13,34 +13,7 @@ import argparse
 
 
 #to make process of sepalating Unit
-def convert(data,pixeldef):
-    pixel = args.pixel
-    UNIT_X,UNIT_Y = pixel,pixel
-    try:
-        fname,f_input,f_output = data
-        save_name = fname.split(".")[0] + ".jpg"
-        print("processing : " + fname)
-        simage = OpenSlide(f_input + "/" + fname)
-        w,h = simage.dimensions
-        w_rep,h_rep = int(w//UNIT_X)+1,int(h//UNIT_Y)+1
-        w_end,h_end = w%UNIT_X,h%UNIT_Y
-        w_size,h_size = UNIT_X,UNIT_Y
-        w_start,h_start = 0,0
-        for i in range(h_rep):
-            if i == h_rep - 1:
-                h_size = h_end
-            for j in range(w_rep):
-                if j == w_rep - 1:
-                    w_size = w_end
-                img = simage.read_region((W_start,h_start),0,(w_size,h_size))
-                img = img.convert("RGB")
-                w_start += UNIT_X
-                img_name = f_name + "_" + i + "_" + j + ".jpg"
-            w_size = UNIT_X
-            h_start += UNIT_Y
-            w_start = 0
-    except:
-        print("Can't open image file : " + fname)
+#def convert(data,pixeldef):
 
 
 if __name__ == "__main__":
@@ -63,8 +36,37 @@ if __name__ == "__main__":
     pixes = args.pixel
     print("----------program start----------")
     #Set multi processing and run.
+    UNIT_X,UNIT_Y = pixes,pixes
+    # insert
+    fname,f_input,f_output = f_list
+    save_name = fname.split(".")[0] + ".jpg"
+    print("processing : " + fname)
+    simage = OpenSlide(f_input + "/" + fname)
+    w,h = simage.dimensions
+    w_rep,h_rep = int(w//UNIT_X)+1,int(h//UNIT_Y)+1
+    w_end,h_end = w%UNIT_X,h%UNIT_Y
+    w_size,h_size = UNIT_X,UNIT_Y
+    w_start,h_start = 0,0
+    for i in range(h_rep):
+        if i == h_rep - 1:
+            h_size = h_end
+        for j in range(w_rep):
+            if j == w_rep - 1:
+                w_size = w_end
+            img = simage.read_region((W_start,h_start),0,(w_size,h_size))
+            img = img.convert("RGB")
+            w_start += UNIT_X
+            img_name = f_name + "_" + i + "_" + j + ".jpg"
+        w_size = UNIT_X
+        h_start += UNIT_Y
+        w_start = 0
+"""
+    except:
+        print("Can't open image file : " + fname)
+
     try:
         convert(f_lst,pixes)
         #testjpg = slide.read_region((0,0),0,slide.dimensions)
     except:
         print("WARNING!!!----------This command was failed-----------")
+"""
