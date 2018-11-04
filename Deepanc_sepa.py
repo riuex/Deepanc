11c53f9f8c1f15386f9c141b830eda9b226ad4c7
biolupus <pestefighter@gmail.com>
2018-11-05 02:56:49 +0900

    I couldn't find
    I couldn't find how they separate the images and select meaning full one.

--- Deepanc_sepa.py
+++ Deepanc_sepa.py
@@ -10,9 +10,44 @@ import multiprocessing as multi
 from PIL import Image
 import argparse
 
-def selgro(image):
+def selgro(image,level,address):
+    tile = image.get_tile(level,address)
 
-    except
+
+def conv2():
+    try UNIT_X,UNIT_Y = wpixels,hpixels
+    fname,f_input,f_output = data
+    save_name = fname.splot("/")[1]
+    save_name = save_name.split(".")[0]
+    print("processing : " + fname)
+    simage = OpenSlide(fname)
+    w,h = simage.dimensions
+        w_rep,h_rep = int(w//UNIT_X)+1,int(h//UNIT_Y)+1
+        w_end,h_end = w%UNIT_X,h%UNIT_Y
+        w_size,h_size = UNIT_X,UNIT_Y
+        w_start,h_start = 0,0
+        print("function is ready!!")
+        for i in range(h_rep):
+            if i == h_rep - 1:
+                h_size = h_end
+            for j in range(w_rep):
+                if j == w_rep - 1:
+                    w_size = w_end
+                img = simage.read_region((w_start,h_start),0,(w_size,h_size))
+                img = img.convert("RGB")
+                img_name = f_output+ "/" + save_name + "_" + str(i) + "_" + str(j) + ".jpg"
+                img.save(img_name)
+                """
+                ggray = img.point()
+                print(ggray)
+                """
+                print("saving image:" + f_output + "/" + save_name + "_(" + str(i) + "/" + str(h_rep-1) + ")_(" + str(j) + "/" + str(w_rep-1) + ").jpg")
+                w_start += UNIT_X
+            w_size = UNIT_X
+            h_start += UNIT_Y
+            w_start = 0
+    except:
+        print("Couldn't run! Something wrong in :" + fname)
 
 
 #to make process of sepalating Unit
