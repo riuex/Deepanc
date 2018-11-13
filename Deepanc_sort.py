0eb660da800a859e27a393cbfc64dc95558b122d
biolupus <pestefighter@gmail.com>
2018-11-14 03:52:53 +0900

    Sepalating system is not completed!
    I should consume about multiprocessing and XML naming

--- Deepanc_sort.py
+++ Deepanc_sort.py
@@ -9,11 +9,14 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description ="This script is ...",formatter_class = argparse.RawTextHelpFormatter )
-    parser.add_argument("folderpath","-f" ,default=input,
-                        help ="you should input the folder name")
+    parser.add_argument("--folderpath","-f" ,default="input",type = str,
+        help ="you should input the folder name")
+    parser.add_argument("--JsonFile","-j",default = "input.json",type = str,
+        help="please input Json file with these images")
 
 
     args = parser.parser_args()
     sourcefolder = os.path.abspath(args.folderpath)
     print("------reading folder of " + args.folderpath)
-    
+
+    JsonFile = args.JsonFile
